59c36c6a236698d2e9d758692cb6b7ee4195fb8c
Juan Barezzi <juan.barezzi@gmail.com>
2022-11-27 18:28:52 -0300

    feat: import fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,16 @@
+import { useFonts } from "expo-font";
 
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-SemiBold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if(!isFontsLoaded) {
+    return null;
+  }
   return (
     
   );
