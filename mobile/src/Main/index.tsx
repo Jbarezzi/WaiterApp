a53662925e4a72450452167a434f6e76abdeaf2f
Juan Barezzi <juan.barezzi@gmail.com>
2022-12-01 18:00:16 -0300

    feat: create button component and implement footer

--- mobile/src/Main/index.tsx
+++ mobile/src/Main/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Header';
 import { CategoriesContainer, Container, Footer, FooterContainer, MenuContainer } from './style';
@@ -16,7 +17,11 @@ export function Main() {
 
       </Container>
       <Footer>
-        <FooterContainer></FooterContainer>
+        <FooterContainer>
+          <Button onPress={() => null}>
+            Novo Pedido
+          </Button>
+        </FooterContainer>
       </Footer>
     </>
   );
--- mobile/src/Main/style.ts
+++ mobile/src/Main/style.ts
@@ -1,5 +1,5 @@
-import { Platform, StatusBar } from "react-native";
-import styled from "styled-components/native";
+import { Platform, StatusBar } from 'react-native';
+import styled from 'styled-components/native';
 
 const isAndroid = Platform.OS === 'android';
 
@@ -21,6 +21,7 @@ export const MenuContainer = styled.View`
 export const Footer = styled.View`
   min-height: 110px;
   background-color: #fff;
+  padding: 16px 24px;
 `;
 
 export const FooterContainer = styled.SafeAreaView``;
